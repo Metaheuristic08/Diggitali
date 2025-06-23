e55841d72b342e5dcdf4d236946531d5bceddc7a
Walter <72948318+1toe@users.noreply.github.com>
2025-06-23 15:52:01 -0400

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,9 +3,10 @@ import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+//(A) IzaSyCTvT6xz3ehX-eA31pcTXbJ26JVOSRI9bo
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCTvT6xz3ehX-eA31pcTXbJ26JVOSRI9bo",
+  apiKey: "",
   authDomain: "ladico-80eb7.firebaseapp.com",
   projectId: "ladico-80eb7",
   storageBucket: "ladico-80eb7.firebasestorage.app",
